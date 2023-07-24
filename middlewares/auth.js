1440e2b8f254f01f4408ed50b89b465740885a78
Lopakadude <lopakadude@gmail.com>
2023-07-24 15:03:59 +0300

    fix: err

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,6 @@ module.exports = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, process.env.NODE_ENV !== 'production' ? 'super-strong-secret' : process.env.JWT_SECRET);
-    console.log(payload);
   } catch (err) {
     throw new AuthorizationError('Авторизация не пройдена. Неверный почта или пароль');
   }
