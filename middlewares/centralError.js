925a73d67b9c2fb8d5791eb3383fb95cdefecf0f
Lopakadude <lopakadude@gmail.com>
2023-07-24 23:39:10 +0300

    test

--- middlewares/centralError.js
+++ middlewares/centralError.js
@@ -1,6 +1,5 @@
 module.exports = (err, req, res, next) => {
   if (!err.statusCode) {
-    console.error;
     return res.status(500).send({'На сервере произошла ошибка'});
   }
   res.status(err.statusCode).send({ message: err.message });
