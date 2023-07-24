0f2b2d480b671baaf4bae972a14763d7a4e530ec
Lopakadude <lopakadude@gmail.com>
2023-07-25 00:29:22 +0300

    feat: refactoring test4

--- middlewares/centralError.js
+++ middlewares/centralError.js
@@ -1,6 +1,5 @@
 module.exports = (err, req, res, next) => {
   if (!err.statusCode) {
-    // eslint-disable-next-line no-console
     return res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
   res.status(err.statusCode).send({ message: err.message });
